1d61e1780b5acc0b184c278bc8618315b650ceda
Colin McEwan <colin.mcewan@gmail.com>
2021-05-15 20:44:27 +0100

    Set 'LANG' and 'LANGUAGE' in case existing environment defines both

--- generate_images/get_all_images.sh
+++ generate_images/get_all_images.sh
@@ -2,7 +2,7 @@
 
 for l in en fr de ca es it nl ; do
   echo "=== Screenshots for $l ==="
-  env LANG="$l" ./get_images.sh "$1"
+  env LANGUAGE="$l" LANG="$l" ./get_images.sh "$1"
   mkdir "generated_$l"
   for p in *.png; do
     mv "$p" "generated_$l"
