3bff60f4304c7204c184e73d00a31a5270c5332a
Colin McEwan <colin.mcewan@gmail.com>
2021-05-18 19:22:12 +0100

    Translation screenshots (#61)
    * Add translated auto screenshots
    * Add language-specific generated images
    * Transform generated_en/ -> generated_$lang/

---
+++ generate_images/get_all_images.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for l in en fr de ca es it nl ; do
+  echo "=== Screenshots for $l ==="
+  env LANGUAGE="$l" LANG="$l" ./get_images.sh "$1"
+  mkdir "generated_$l"
+  for p in *.png; do
+    mv "$p" "generated_$l"
+  done
+done
+
