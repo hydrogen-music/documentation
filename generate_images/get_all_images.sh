3df8f8037f7fd3848994ebede288b66e4a02c56b
theGreatWhiteShark <princess.trudildis@posteo.de>
2022-08-10 13:29:02 +0200

    v1.2 update images
    updating all image before string freeze

--- generate_images/get_all_images.sh
+++ generate_images/get_all_images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for l in en ; do #fr de ca es it nl ; do
+for l in en fr de ca es it nl ; do
   echo "=== Screenshots for $l ==="
   env LANGUAGE="$l" LANG="$l" ./get_images.sh "$1"
   mkdir "../img/generated_$l"
